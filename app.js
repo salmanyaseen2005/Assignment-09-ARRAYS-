d5e01ea5d4c1affa44d27af2ba6e90fdabe489a1
salmanyaseen2005 <salmanyaseen9090@gmai.com>
2023-12-10 04:43:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,129 @@
+// // Quesation 01
+
+// var studentNames={}
+ 
+// studentNames[0]="ali"
+// studentNames[1]="salman"
+// studentNames[2]="hassan"
+
+// console.log(studentNames)
+
+// // Quesation 02
+
+// var studentNames=[]
+ 
+// studentNames.push("ali")
+// studentNames.push("salman")
+// studentNames.push("hassan")
+// console.log(studentNames)
+
+// //  Quesation 03
+
+// var name=["ali","salman","hassan"]
+// console.log(name)
+
+// //  Quesation 04
+
+// var number=[1,2,3,4]
+
+// console.log(number)
+
+// //  Quesation 05
+
+// var boolean=[true,false]
+
+// console.log(boolean)
+
+// //  Quesation 06
+
+// var  mixed=["salman",1,true]
+
+// console.log(mixed)
+
+// //  Quesation 07
+
+
+// var qualifications=["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"];
+
+//  document.write("<h1>Qualifications</h1>");
+//  document.write("<ol>")
+// document.write(`<li>${qualifications[0]}</li><li>${qualifications[1]}</li>`)
+// document.write(`<li>${qualifications[2]}</li><li>${qualifications[3]}</li>`)
+// document.write(`<li>${qualifications[4]}</li><li>${qualifications[5]}</li>`)
+// document.write(`<li>${qualifications[6]}</li><li>${qualifications[7]}</li>`)
+// document.write("</ol>")
+
+// //  Quesation 08
+
+// var student=["ali","salman","hassan"]
+
+// var scores=[320,230,480]
+
+// var total=500
+
+
+// var percentages=(scores[0]/total)*100
+// var percentages1=(scores[1]/total)*100
+// var percentages2=(scores[2]/total)*100
+
+// document.write("Score of "+student[0]+" is "+scores[0]+" percentages "+percentages+"%")
+// document.write("<br>"+"Score of "+student[1]+" is "+scores[1]+" percentages "+percentages1+"%")
+// document.write("<br>"+"Score of "+student[2]+" is "+scores[2]+" percentages "+percentages2+"%")
+// //  Quesation 09
+// //  Quesation 10
+
+// var scores=[320,230,480,120]
+
+// scores.sort()
+
+
+
+// document.write("<br>"+scores)
+
+
+
+
+
+// //  Quesation 11
+
+// var  cites=["karachi","lahore","islamabad","quetta","peshawar"]
+
+//     console.log(cites[2]+cites[3])
+
+// //  Quesation 12
+//     var arr = ["This ,  is ,   my , cat"];
+
+//     var string=["This is my Cat"]
+
+//     document.write("<h2>arr</h2>"+arr)
+//     document.write("<h2>string</h2>"+string)
+
+
+// //  Quesation 13
+
+var devices=["Keyboard","Mouse","Printer","Monitor"]
+
+document.write(devices.shift()+ "<br>");
+document.write(devices.shift()+ "<br>");
+document.write(devices.shift()+ "<br>");
+document.write(devices.shift()+ "<br>");
+
+document.write( "<br>");
+
+// //  Quesation 14
+ var devices=["Keyboard","Mouse","Printer","Monitor"]
+document.write(devices.pop()+ "<br>");
+document.write(devices.pop()+ "<br>");
+document.write(devices.pop()+ "<br>");
+document.write(devices.pop()+ "<br>");
+
+
+
+
+// var student=["ali","salman","fatima","hassan","saud"]
+
+// var copy=student.slice(2,4)
+
+// // student.slice(1,2,)
+
+// doi(copy)
